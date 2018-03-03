db160fcc417357b427f10d3835f3c7dec832a597
Oleg 'legionem' Aleksandrov <smith5989@gmail.com>
2018-03-03 17:53:58 +0300

    remove caching

--- src/main/java/com/movie/access/MovieAccessApplication.java
+++ src/main/java/com/movie/access/MovieAccessApplication.java
@@ -1,41 +1,12 @@
 package com.movie.access;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.movie.access.system.errors.TheMovieDBOperationException;
-import com.movie.access.system.shared.MovieInfo;
-import com.movie.access.system.themoviedb.operations.TheMovieDBOperationExecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class MovieAccessApplication
 {
-    @Bean
-    public LoadingCache<Integer, MovieInfo> createMovieCache()
-    {
-        return CacheBuilder.newBuilder()
-                .concurrencyLevel(4)
-                .weakKeys()
-                .maximumSize(10000)
-                .expireAfterWrite(10, TimeUnit.MINUTES)
-                .build(new CacheLoader<Integer, MovieInfo>()
-                {
-                    public MovieInfo load(Integer movieId) throws URISyntaxException,
-                                                              IOException,
-                                                              TheMovieDBOperationException
-                    {
-                        return TheMovieDBOperationExecutor.getMovieInfo(movieId);
-                    }
-                });
-    }
-
 	public static void main(String[] args)
     {
 		SpringApplication.run(MovieAccessApplication.class, args);
--- src/main/java/com/movie/access/web/MainController.java
+++ src/main/java/com/movie/access/web/MainController.java
@@ -1,11 +1,9 @@
 package com.movie.access.web;
 
-import com.google.common.cache.LoadingCache;
 import com.movie.access.system.errors.TheMovieDBOperationException;
 import com.movie.access.system.shared.MovieInfo;
 import com.movie.access.system.shared.MovieList;
 import com.movie.access.system.themoviedb.operations.TheMovieDBOperationExecutor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,21 +14,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/movie")
 @EnableWebMvc
 public class MainController
 {
-    @Autowired
-    LoadingCache<Integer, MovieInfo> movieCache;
-
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     @ResponseBody
-    public MovieInfo getMovieInfo(@RequestParam int movieId) throws ExecutionException
+    public MovieInfo getMovieInfo(@RequestParam int movieId) throws URISyntaxException, IOException, TheMovieDBOperationException
     {
-        return movieCache.get(movieId);
+        return TheMovieDBOperationExecutor.getMovieInfo(movieId);
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
